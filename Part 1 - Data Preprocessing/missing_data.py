01d70621c0454a7146f9f16e6a476401bb1129d3
Bikash <bikash11roy@gmail.com>
2017-05-26 19:59:43 +0600

    Handling missing data with mean

--- Part 1 - Data Preprocessing/missing_data.py
+++ Part 1 - Data Preprocessing/missing_data.py
@@ -11,7 +11,9 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
+# Strategy is to replace the column where there is missing data with mean of that column0
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+imputer = Imputer(missing_values='NaN', strategy= 'mean', axis=0)
+
+# fitting the imputer in out data set
+imputer.fit(X[:, 1:3])
