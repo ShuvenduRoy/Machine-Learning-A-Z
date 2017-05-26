ff9b98c3e7039b1f93cce9d762d711d9c83a9d73
Bikash <bikash11roy@gmail.com>
2017-05-27 01:34:19 +0600

    Preparing data for linear regression modeling in R

---
+++ 2 - Regression/1  - Simple Linear Regression/1. Simple Linear Regression.R
@@ -0,0 +1,17 @@
+# Data Preprocessing
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+
+# Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
