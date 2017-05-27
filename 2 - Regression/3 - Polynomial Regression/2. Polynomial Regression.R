3d3fb54f426ebef140cf76b8e2e8838f405f1be3
Bikash <bikash11roy@gmail.com>
2017-05-28 01:06:20 +0600

    Fitting polynomial regression to the dataset in R

--- 2 - Regression/3 - Polynomial Regression/2. Polynomial Regression.R
+++ 2 - Regression/3 - Polynomial Regression/2. Polynomial Regression.R
@@ -3,3 +3,18 @@
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
+
+
+# Create a linear model for comperasion
+lin_reg = lm(formula = Salary ~ ., 
+             data = dataset)
+summary(lin_reg)
+
+
+# Fitting polynomial regression to the dataset
+dataset$Level2 = dataset$Level ^ 2
+dataset$Level3 = dataset$Level ^ 3
+poly_reg = lm(formula = Salary ~ .,
+              data = dataset)
+summary(poly_reg)
+
