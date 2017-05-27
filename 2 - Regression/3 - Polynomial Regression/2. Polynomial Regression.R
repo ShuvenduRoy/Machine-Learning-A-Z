758781e46c525da59a6f0cdb6656f67fe95b1cdb
Bikash <bikash11roy@gmail.com>
2017-05-28 00:52:28 +0600

    Getting data in R for modeling

--- 2 - Regression/3 - Polynomial Regression/2. Polynomial Regression.R
+++ 2 - Regression/3 - Polynomial Regression/2. Polynomial Regression.R
@@ -1,17 +1,5 @@
 # Data Preprocessing
 
 # Importing the dataset
-dataset = read.csv('Data.csv')
-
-
-# install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
-
-
-# Feature Scaling
-# training_set[, 2:3] = scale(training_set[, 2:3])
-# test_set[, 2:3] = scale(test_set[, 2:3])
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
