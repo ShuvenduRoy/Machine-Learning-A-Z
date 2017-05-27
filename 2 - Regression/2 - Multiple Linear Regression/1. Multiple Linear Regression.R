10d389e77c64ffd5bf8573c2e14bfbb5a609a402
Bikash <bikash11roy@gmail.com>
2017-05-27 15:08:25 +0600

    Data processed for model fitting in R

---
+++ 2 - Regression/2 - Multiple Linear Regression/1. Multiple Linear Regression.R
@@ -0,0 +1,17 @@
+# Data Preprocessing
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+dataset$State = factor(dataset$State,
+                         levels = c('New York', 'California', 'Florida'),
+                         labels = c(1, 2, 3))
+
+
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
