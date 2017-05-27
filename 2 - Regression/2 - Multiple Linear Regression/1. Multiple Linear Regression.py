2d20a81a5c57e3c0c3313791625c8fe5dce9d850
Bikash <bikash11roy@gmail.com>
2017-05-27 13:11:32 +0600

    Preparing data for MLR in python

---
+++ 2 - Regression/2 - Multiple Linear Regression/1. Multiple Linear Regression.py
@@ -0,0 +1,38 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Handling categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+one_hot_encoder = OneHotEncoder(categorical_features = [3])
+X = one_hot_encoder.fit_transform(X).toarray()
+
+
+# Avoiding summy variable trap
+X = X[:, 1:]
+
+
+
+# Splitting the dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# print(X_train, X_test, y_train, y_test)
+
+
+"""# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+print(X_train)
+print(X_test)
+"""
