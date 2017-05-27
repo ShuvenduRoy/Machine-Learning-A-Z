4a9cb67d08bd9b1660c1bc3414d91994d6b024ea
Bikash <bikash11roy@gmail.com>
2017-05-28 00:04:42 +0600

    Viaualizing Polunomial vs linear regression

--- 2 - Regression/3 - Polynomial Regression/2. Polynomial Regression.py
+++ 2 - Regression/3 - Polynomial Regression/2. Polynomial Regression.py
@@ -16,8 +16,26 @@ lin_reg.fit(X, y)
 
 # Fitting Polunomial regression in model
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 4)
+poly_reg = PolynomialFeatures(degree = 3)
 X_poly = poly_reg.fit_transform(X)
 
 lin_reg2 = LinearRegression()
 lin_reg2.fit(X_poly, y)
+
+# Visualization of Linear regression
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title('Truth or Bluff(Linear Regression)')
+plt.xlabel('Position leevel')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Visualization of Poinimial Regression
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)), color='blue')
+plt.title('Truth or Bluff(Polinomial Regression)')
+plt.xlabel('Position leevel')
+plt.ylabel('Salary')
+plt.show()
+
