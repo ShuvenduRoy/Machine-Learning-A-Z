ffccf6ef2e1d8011266bce31dd7537c1be8b5f57
Bikash <bikash11roy@gmail.com>
2017-05-28 01:32:06 +0600

    Visualizing polynomial regression to the dataset in R

--- 2 - Regression/3 - Polynomial Regression/2. Polynomial Regression.R
+++ 2 - Regression/3 - Polynomial Regression/2. Polynomial Regression.R
@@ -18,3 +18,19 @@ poly_reg = lm(formula = Salary ~ .,
               data = dataset)
 summary(poly_reg)
 
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             color = 'red') + 
+  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
+            color = 'blue') +
+  ggtitle('Truth or Bluff (Linear Regression)')
+
+
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             color = 'red') + 
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
+            color = 'blue') +
+  ggtitle('Truth or Bluff (Polynomial Regression)')
