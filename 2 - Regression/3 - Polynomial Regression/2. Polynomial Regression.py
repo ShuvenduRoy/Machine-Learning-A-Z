f2d2e282f2b479cb6a59bebbf49e95bc6061f725
Bikash <bikash11roy@gmail.com>
2017-05-27 23:51:36 +0600

    Fitting Polunomial regression in model

--- 2 - Regression/3 - Polynomial Regression/2. Polynomial Regression.py
+++ 2 - Regression/3 - Polynomial Regression/2. Polynomial Regression.py
@@ -9,3 +9,15 @@ X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 
+# Fitting linear regression in model
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polunomial regression in model
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+
+lin_reg2 = LinearRegression()
+lin_reg2.fit(X_poly, y)
