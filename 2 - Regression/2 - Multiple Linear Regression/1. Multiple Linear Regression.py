014e637e04ad00bd0b6a479418e72fe3cd665166
Bikash <bikash11roy@gmail.com>
2017-05-27 13:53:45 +0600

    Predicting with test data

--- 2 - Regression/2 - Multiple Linear Regression/1. Multiple Linear Regression.py
+++ 2 - Regression/2 - Multiple Linear Regression/1. Multiple Linear Regression.py
@@ -32,3 +32,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 from sklearn.linear_model import LinearRegression 
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+
+# Predicting with the model
+y_pred = regressor.predict(X_test)
