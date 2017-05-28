84b848f9b2a56ad345c24989944c9ec2fa757cb8
Bikash <bikash11roy@gmail.com>
2017-05-28 20:42:56 +0600

    fitting logistic regression to the train set

--- 3 - Classification/1 - Logistic Regression/logistic_regression.py
+++ 3 - Classification/1 - Logistic Regression/logistic_regression.py
@@ -22,3 +22,7 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.fit_transform(X_test)
 
 
+# Fitting Logistic Regression to the training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
