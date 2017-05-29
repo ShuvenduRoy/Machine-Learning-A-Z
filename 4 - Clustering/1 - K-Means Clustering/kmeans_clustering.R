7fbe9494a0a79382278c13db626912fafeb02207
Bikash <bikash11roy@gmail.com>
2017-05-30 02:20:37 +0600

    Getting the optimal number of cluster in R

--- 4 - Clustering/1 - K-Means Clustering/kmeans_clustering.R
+++ 4 - Clustering/1 - K-Means Clustering/kmeans_clustering.R
@@ -2,3 +2,11 @@
 
 dataset <- read.csv('mall.csv')
 x <- dataset[4:5]
+
+
+# Using the elbow method to find the optimal number of clusters
+set.seed(6)
+wcss <- vector()
+for (i in 1:10) wcss[i] <- sum(kmeans(x, i)$withinss)
+plot(1:10, wcss, type = 'b')
+
