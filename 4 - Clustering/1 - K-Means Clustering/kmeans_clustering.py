6ad825ae4346c69985f6882faffd09e0789b6217
Bikash <bikash11roy@gmail.com>
2017-05-30 02:06:12 +0600

    Visualizing Cluster

--- 4 - Clustering/1 - K-Means Clustering/kmeans_clustering.py
+++ 4 - Clustering/1 - K-Means Clustering/kmeans_clustering.py
@@ -24,3 +24,17 @@ plt.show()
 # Applying k-means to the mall database
 kmeans = KMeans(n_clusters=5, random_state=0)
 y_kmeans = kmeans.fit_predict(X)
+
+
+# Visualising the clusters
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c='red', label = 'Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c='blue', label = 'Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c='green', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c='cyan', label = 'Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c='magenta', label = 'Cluster 5')
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c='Yellow', label = 'Cnteroid')
+plt.title('Clusters of Clients')
+plt.legend()
+plt.show()
+
