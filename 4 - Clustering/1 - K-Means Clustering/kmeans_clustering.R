2aaeb303c4d92505111a0a3f0fc3614b133c206f
Bikash <bikash11roy@gmail.com>
2017-05-30 02:24:35 +0600

    Visualizing the clusters

--- 4 - Clustering/1 - K-Means Clustering/kmeans_clustering.R
+++ 4 - Clustering/1 - K-Means Clustering/kmeans_clustering.R
@@ -10,3 +10,19 @@ wcss <- vector()
 for (i in 1:10) wcss[i] <- sum(kmeans(x, i)$withinss)
 plot(1:10, wcss, type = 'b')
 
+# applying k-means to the mall
+kmeans <- kmeans(x, 5, iter.max = 300, nstart = 10)
+
+
+# Vialuslizing the cluesters
+library(cluster)
+clusplot(x,
+         kmeans$cluster,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of clients')
+         )
