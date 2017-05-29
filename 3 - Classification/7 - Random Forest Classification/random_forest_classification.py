c92536949a1c094babb759f5949bae26cef7ac06
Bikash <bikash11roy@gmail.com>
2017-05-29 21:21:13 +0600

    Accuracy function in sklearn

--- 3 - Classification/7 - Random Forest Classification/random_forest_classification.py
+++ 3 - Classification/7 - Random Forest Classification/random_forest_classification.py
@@ -11,7 +11,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -31,6 +31,10 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, y_pred))
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
