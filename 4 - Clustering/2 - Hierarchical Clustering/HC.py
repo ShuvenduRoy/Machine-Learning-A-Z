1c40eea38d490e509ed024241455251b796f3fe7
Bikash <bikash11roy@gmail.com>
2017-05-30 03:29:19 +0600

    Fitting RC to the mall data set

--- 4 - Clustering/2 - Hierarchical Clustering/HC.py
+++ 4 - Clustering/2 - Hierarchical Clustering/HC.py
@@ -10,6 +10,11 @@ X = dataset.iloc[:, [3,4]].values
 # Using the dendeogram to find the optimal of cluster
 import scipy.cluster.hierarchy as sch
 dendogram = sch.dendrogram(sch.linkage(X, method='ward'))
+plt.show()
 
 
-                 
+# Fitting RC to the mall data set
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters=5, affinity="euclidean", linkage='ward')
+y_hc = hc.fit_predict(X)
+
