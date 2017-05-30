10b20b247ab9142506d9bf45ec2bad54f3fbd750
Bikash <bikash11roy@gmail.com>
2017-05-30 19:20:01 +0600

    Visualizing apriori in R

--- 5 - Association Rule Learning/1 - Apriori/apriori.R
+++ 5 - Association Rule Learning/1 - Apriori/apriori.R
@@ -4,6 +4,14 @@
 # install.packages('arules')
 library(arules)
 dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
+
+# Converting into sparse matrix
 dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
 summary(dataset)
 itemFrequencyPlot(dataset, topN = 10)
+
+# Training Apriori on the dataset
+rules = apriori(data = dataset, parameter = list(support=0.003, confidence = 0.4))
+
+# visualizing the results 
+inspect(sort(rules, by = 'lift')[1:10])
