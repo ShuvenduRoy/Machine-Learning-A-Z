26c67696caf0419c50626dd88ce33a2f1efedb63
Bikash <bikash11roy@gmail.com>
2017-05-31 05:55:31 +0600

    handling categorical values

--- 8 - Deep Learning/1 - Artificial Neural Networks (ANN)/Artificial Neural Networks.py
+++ 8 - Deep Learning/1 - Artificial Neural Networks (ANN)/Artificial Neural Networks.py
@@ -10,6 +10,20 @@ dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
+# Changing the categorical data into numerical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X_1 = LabelEncoder()
+X[:, 1] = labelEncoder_X_1.fit_transform(X[:, 1])
+
+labelEncoder_X_2 = LabelEncoder()
+X[:, 2] = labelEncoder_X_2.fit_transform(X[:, 2])
+
+onehotencoder = OneHotEncoder(categorical_features=[1])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding dummy variablee trap
+X = X[:, 1:]
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
