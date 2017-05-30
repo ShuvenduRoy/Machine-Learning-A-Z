857cb1035a4eb7e167061231311206abf8b008a6
Bikash <bikash11roy@gmail.com>
2017-05-31 01:54:26 +0600

    Natural language Processing

--- 7 - Natural Language Processing/Natural_language_processing.py
+++ 7 - Natural Language Processing/Natural_language_processing.py
@@ -34,3 +34,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer()
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+ 
