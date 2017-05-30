3fc5be107848768b04ce56bdacb606489eb5db70
Bikash <bikash11roy@gmail.com>
2017-05-30 23:38:15 +0600

    Spliting the lines into words

--- 7 - Natural Language Processing/Natural_language_processing.py
+++ 7 - Natural Language Processing/Natural_language_processing.py
@@ -5,12 +5,14 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3) # Ignoring the double quote
 
+# Getting the stopwords
+import nltk
+nltk.download('stopwords')
+
 # Cleaning the text
 import re
 # Keeping the letters only
 review = re.sub('[^a-zA-Z]',' ', dataset['Review'][0])
 # Kepping the lower letter only
 review = review.lower()
-
-import nltk
-nltk.download('stopwords')
+review = review.split()
