90236820be142bcb9fb319a4d36bfc34a795d26e
Bikash <bikash11roy@gmail.com>
2017-05-31 01:12:43 +0600

    Cleaning all sentences

--- 7 - Natural Language Processing/Natural_language_processing.py
+++ 7 - Natural Language Processing/Natural_language_processing.py
@@ -13,13 +13,17 @@ from nltk.corpus import stopwords
 
 # Cleaning the text
 import re
-# Keeping the letters only
-review = re.sub('[^a-zA-Z]',' ', dataset['Review'][0])
-# Keeping the lower letter only
-review = review.lower()
-review = review.split()
 
-ps = PorterStemmer()
-review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
-
-
+corpus = []
+for i in range(0, 1000):
+    # Keeping the letters only
+    review = re.sub('[^a-zA-Z]',' ', dataset['Review'][i])
+    # Keeping the lower letter only
+    review = review.lower()
+    review = review.split()
+    
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    
+    review = ' '.join(review)
+    corpus.append(review)
