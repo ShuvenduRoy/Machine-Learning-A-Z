ae0aad5db3201579de218a842c3f6a57114e2a67
Bikash <bikash11roy@gmail.com>
2017-05-31 22:13:40 +0600

    Step 4 - Full Connection

--- 8 - Deep Learning/2 - Convolutional Neural Networks (CNN)/cnn.py
+++ 8 - Deep Learning/2 - Convolutional Neural Networks (CNN)/cnn.py
@@ -25,11 +25,13 @@ classifier = Sequential()
 # Step 1 - Convolution
 classifier.add(Convolution2D(32, 3, 3, input_shape= (64,64,3), activation= 'relu'))
 
-# Step 2: Polling
+# Step 2 - Polling
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-# Step 3: Flattening
+# Step 3 - Flattening
 classifier.add(Flatten())
 
-
+# Step 4 - Full Connection
+classifier.add(Dense(output_dim = 128, activation='relu'))
+classifier.add(Dense(output_dim = 1, activation='sigmoid'))
 
