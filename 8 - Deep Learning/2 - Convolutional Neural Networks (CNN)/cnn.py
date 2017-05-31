f91aefffce14b58d9aada6531b5e9ddf73163039
Bikash <bikash11roy@gmail.com>
2017-05-31 20:48:32 +0600

    Set Up for convolutional neural network

---
+++ 8 - Deep Learning/2 - Convolutional Neural Networks (CNN)/cnn.py
@@ -0,0 +1,14 @@
+# Convolutional Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
