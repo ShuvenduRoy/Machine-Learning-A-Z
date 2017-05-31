a772c57fe901901b87a20ec2767decadedceba33
Bikash <bikash11roy@gmail.com>
2017-05-31 20:59:36 +0600

    Importing dependencies

--- 8 - Deep Learning/2 - Convolutional Neural Networks (CNN)/cnn.py
+++ 8 - Deep Learning/2 - Convolutional Neural Networks (CNN)/cnn.py
@@ -12,3 +12,9 @@
 # Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
