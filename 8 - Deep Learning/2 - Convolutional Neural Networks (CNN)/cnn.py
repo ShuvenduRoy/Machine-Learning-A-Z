4c5ef294c82f9d3a08a5312a74510dbd2c48f62f
Bikash <bikash11roy@gmail.com>
2017-05-31 22:48:05 +0600

    Part 2 - Fitting the CNN to the images

--- 8 - Deep Learning/2 - Convolutional Neural Networks (CNN)/cnn.py
+++ 8 - Deep Learning/2 - Convolutional Neural Networks (CNN)/cnn.py
@@ -39,3 +39,32 @@ classifier.add(Dense(output_dim = 1, activation='sigmoid'))
 # Compiling the CNN
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+
+# Part 2 - Fitting the CNN to the images
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size=(64, 64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+        'dataset/test_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+
+classifier.fit_generator(
+        training_set,
+        steps_per_epoch=8000,
+        epochs=25,
+        validation_data=test_set,
+        validation_steps=2000)
