b1b795aff8da78cff2a8ddf7904fa53a0eb09935
Bikash <bikash11roy@gmail.com>
2017-05-31 06:00:31 +0600

    Getting packages ready

--- 8 - Deep Learning/1 - Artificial Neural Networks (ANN)/Artificial Neural Networks.py
+++ 8 - Deep Learning/1 - Artificial Neural Networks (ANN)/Artificial Neural Networks.py
@@ -26,7 +26,7 @@ X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -34,8 +34,13 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+
+# Step 2 : Making the ANN
 # Fitting classifier to the Training set
-# Create your classifier here
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
